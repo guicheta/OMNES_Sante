60615b5e7d847c8e6f300de52d75de7ab43b279b
guicheta <104624937+guicheta@users.noreply.github.com>
2022-05-27 16:22:21 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -56,40 +56,83 @@ $telephone = mysqli_real_escape_string($conn, $telephone);
 }
 ?>
 <div class="inscription">
-<h1>Inscrivez-vous</h1>
-<p>Merci de rentrer vos informations personelles</p>
-<form action="connection.php" method="post">
-    <div>
-        <label for="id">Votre ID</label>
-        <input type="text" name="id" placeholder="1111" required>
-    </div>
-<div>
-    <label for="nom">Votre nom</label>
-    <input type="text" name="nom" placeholder="Dupond" required>
-</div>
-<div>
-    <label for="prenom">Votre prénom</label>
-    <input type="text" name="prenom" placeholder="Martin" required>
-</div>
-<div>
-    <label for="email">Votre e-mail</label>
-    <input type="email" name="email" placeholder="martin.dupond@mail.com" required>
-</div>
-<div>
-    <label for="motdepasse">Votre mot de passe</label>
-    <input type="password" name="motdepasse" placeholder="Chouchou82" required>
-</div>
-<div>
-    <label for="tel">Votre numéro de téléphone</label>
-    <input type="text" name="tel" placeholder="06 45 65 67 21" required>
-</div>
-<div>
-    <button type="submit" id="sub">S'inscrire</button>
-    <a href="connection.php" target="_self" id="co">
-        Déjà inscrit ?
-    </a>
-</div>
-</form>
+
+    <h1>Inscrivez-vous</h1>
+
+    <p>Merci de rentrer vos informations personelles</p>
+
+    <form action="connection_client.php" method="post">
+
+        <div>
+            <label for="id">Votre ID</label>
+            <input type="text" name="id" placeholder="1111" required>
+        </div>
+        <div>
+            <label for="nom">Votre nom</label>
+            <input type="text" name="nom" placeholder="Dupond" required>
+        </div>
+        <div>
+            <label for="prenom">Votre prénom</label>
+            <input type="text" name="prenom" placeholder="Martin" required>
+        </div>
+        <div>
+            <label for="email">Votre e-mail</label>
+            <input type="email" name="email" placeholder="martin.dupond@mail.com" required>
+        </div>
+        <div>
+            <label for="motdepasse">Votre mot de passe</label>
+            <input type="password" name="motdepasse" placeholder="Chouchou82" required>
+        </div>
+        <div>
+            <label for="tel">Votre numéro de téléphone</label>
+            <input type="text" name="tel" placeholder="06 45 65 67 21" required>
+        </div>
+
+        <div>
+            <button type="submit" id="sub">S'inscrire</button>  
+        </div>
+
+        
+            <ul style="display: flex;
+                list-style-type: none;
+                align-items: center;
+                justify-content: center;
+                text-decoration: none;">
+                <li style="padding-left: 0.5em;">
+                    <a href="connection_client.php" target="_self" style="border-radius: 12px;
+        text-decoration: none;
+        padding: 1em 1.2em;
+        color: black;
+        border: 2px solid black;
+        background-color: lightblue;">
+                    Client
+                    </a>
+                </li>
+                <li style="padding-left: 0.5em;">        
+                    <a href="connection_admin.php" target="_self"style="border-radius: 12px;
+        text-decoration: none;
+        padding: 1em 1.2em;
+        color: black;
+        border: 2px solid black;
+        background-color: lightblue; ">
+                    Medecin
+                    </a>
+                </li>
+                <li style="padding-left: 0.5em;">
+                    <a href="connection_admin.php" target="_self"style="border-radius: 12px;
+        text-decoration: none;
+        padding: 1em 1.2em;
+        color: black;
+        border: 2px solid black;
+        background-color: lightblue;" >
+                    Admin
+                    </a>       
+                </li>
+            </ul>
+        
+
+    </form>
+
 </div>
 </body>
 </html>
