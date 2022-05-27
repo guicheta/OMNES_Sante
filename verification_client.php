52cf859ba8af5376a72225404a0956f45ce127e7
guicheta <104624937+guicheta@users.noreply.github.com>
2022-05-27 16:23:42 +0200

    Add files via upload

---
+++ verification_client.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+if(isset($_POST['email']) && isset($_POST['motdepasse']))
+{
+    // connexion à la base de données
+    $mysqli= new mysqli("localhost","root","","omnes_sante");
+    
+    // on applique les deux fonctions mysqli_real_escape_string et htmlspecialchars
+    // pour éliminer toute attaque de type injection SQL et XSS
+    $email = mysqli_real_escape_string($mysqli,htmlspecialchars($_POST['email'])); 
+    $motdepasse = mysqli_real_escape_string($mysqli,htmlspecialchars($_POST['motdepasse']));
+    
+    if($email !== "" && $motdepasse !== "")
+    {
+        $requete = "SELECT count(*) FROM client WHERE
+              mail = '".$email."' and mdp = '".$motdepasse."' ";
+        $exec_requete = mysqli_query($mysqli,$requete);
+        $reponse      = mysqli_fetch_array($exec_requete);
+        $count = $reponse['count(*)'];
+        if($count!=0) // nom d'utilisateur et mot de passe correctes
+        {
+           $_SESSION['email'] = $email;
+           header('Location: index_client.php');
+        }
+        else
+        {
+           header('Location: connection.php?erreur=1'); // utilisateur ou mot de passe incorrect
+        }
+    }
+    else
+    {
+       header('Location: connection.php?erreur=2'); // utilisateur ou mot de passe vide
+    }
+}
+else
+{
+   header('Location: connection.php');
+}
+mysqli_close($mysqli); // fermer la connexion
+?>
