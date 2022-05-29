84e938e5f60d2df0021ee849d0c81beb7a7f8e86
guicheta <104624937+guicheta@users.noreply.github.com>
2022-05-29 19:53:20 +0200

    Add files via upload

---
+++ fiche_med.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>OMNES Santé</title>
+    <meta charset="UTF-8">
+    <link href="fiche_med.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+    <div class="info">
+        <div class="pdp">
+            <img src="photo_médecin/gilesvalin.jpeg"
+                 alt="Photo Contact"
+                 class="contact"
+                 style="width:300px;height:200px;">
+        </div>
+        <div class="contacter">
+            <p>Dr. NOM Prénom</p>
+            <p>Médecin généraliste</p>
+            <p>Salle: P445</p>
+            <p>Téléphone: +33678963522</p>
+            <p>Email: nom.prénom@gmail.com</p>
+        </div>
+        <div class="calendrier" style="height: 300px; width: 500px;">
+            <p>Afficher les dispos</p> 
+        </div>
+    </div>
+    <div class="choix">
+            <ul>
+                <li>
+                    <a href="planning.html"
+                       target="_self">Prendre RDV</a>
+                </li>
+                <li>
+                    <a href="discussion.html"
+                       target="_self">Communiquer avec le médecin</a>
+                </li>
+                <li>
+                    <a href="CV.xml"
+                       target="_self">Consulter le CV</a>
+                </li>
+            </ul>
+    </div>
+</body>
+</html>
---
+++ tout_parcourir.php
@@ -0,0 +1,53 @@
+
+<!DOCTYPE html>
+<html>
+
+    <meta charset="utf-8"/>
+    <link href="tout_parcourir.css" rel="stylesheet" type="text/css" />
+    
+    <head>
+    </head>
+
+    <body>
+        <div id="wrapper">
+            <div id="image">
+                <img src="img/logo_omnes.png"
+                     alt="Logo Omnes Santé"
+                     class="logo" width="280" height="120">
+                  
+            </div>
+            <div id="photo_medecin">
+                <img src = "photo_medecin.png" width="600px" height="300px"  />
+              </div>
+
+            <div id="navigation">
+                <ul>
+                    <li>
+                        <a href="index_client.php"
+                           target="_self">Accueil</a>
+                    </li>
+                    <br>
+                    <li>
+                        <a href="afficher_medecin_g.php"
+                           target="_self">Médecin généraliste</a>
+                    </li>
+                    <br>
+                    <li>
+                        <a href="afficher_medecin_spe.php"
+                           target="_self">Médecin spécialisé</a>
+                    </li>
+                    <br>
+                    <li>
+                        <a href="https://www.snow-online.com/weather/ski-resort/jasna.html"
+                           target="_self">Laboratoire</a>
+                    </li>
+                    
+                </ul>
+        </div>
+
+    </body>
+ </html>      
+
+    
+    
+
